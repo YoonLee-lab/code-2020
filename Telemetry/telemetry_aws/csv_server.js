9f80851dfff3cf1ba62a2aaa6cdabd6c1f3bf193
Ubuntu <ubuntu@ip-172-31-21-176.us-east-2.compute.internal>
2020-07-26 08:52:48 +0000

    allow config refresh

--- Telemetry/telemetry_aws/csv_server.js
+++ Telemetry/telemetry_aws/csv_server.js
@@ -21,7 +21,7 @@ function clean (req) {
   );
 }
 
-app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, 'index.html')));
+app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, 'views/index.html')));
 app.post('/upload', (req, res) => {
 	const db_name = req.body.db_name.trim().replace(/ /g,"_");
 	if (!db_name) return res.redirect('/?err=true');
@@ -29,5 +29,13 @@ app.post('/upload', (req, res) => {
 	child_process.exec(`python3 csv_to_influx.py ${db_name} ./${req.files.csv.file}`, () => {}) //clean(req))
 	res.send(`Your CSV is being written to database ${db_name}. Go to <a href=http://ec2-3-134-2-166.us-east-2.compute.amazonaws.com:3000/>Grafana</a> to view`);
 });
+
+const get_status = () => child_process.execSync('sh ~/update_scripts/status.sh').toString().split('\n').map(x => x.trim()).filter(x => x.length);
+const get_branches = () => child_process.execSync('sh ~/update_scripts/branches.sh').toString().split('\n').filter(x => x.length);
+const restart = branch => get_branches().includes(branch) && child_process.execSync('sh ~/update_scripts/restart.sh').toString();
+
+app.get('/status', (req, res) => res.sendFile(path.resolve(__dirname, 'views/status.html')))
+app.get('/config', (req, res) => { let [ status, current_branch ] = get_status(); res.json({ status, current_branch, branches: get_branches() }); })
+app.post('/refresh', (req, res) => { console.log(req.body); res.redirect('/status'); })
 
 app.listen(PORT, () => console.log("Grafana upload server running on port " + PORT));
